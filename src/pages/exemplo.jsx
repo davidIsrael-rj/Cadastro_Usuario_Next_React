bdc76e4e95f0f29a5777cee0229327588ac53a44
DavidIsrael <davidisrael.rj@gmail.com>
2023-08-13 08:22:40 -0300

    Usando componente

---
+++ src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+    return (
+        <div>
+            <h1>Exemplo</h1>
+            <Cabecalho />
+        </div>
+    )
+}
