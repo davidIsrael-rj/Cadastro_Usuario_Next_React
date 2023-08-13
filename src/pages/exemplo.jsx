3dcdda1b5315c2dc9e849b6754099ce3df802985
DavidIsrael <davidisrael.rj@gmail.com>
2023-08-13 09:23:26 -0300

    Propriedade de Componentes

--- src/pages/exemplo.jsx
+++ src/pages/exemplo.jsx
@@ -1,10 +1,13 @@
 import Cabecalho from "../components/Cabecalho";
 
+
 export default function Exemplo() {
     return (
         <div>
             <h1>Exemplo</h1>
-            <Cabecalho />
+            <Cabecalho titulo="Next.js & React"/>
+            <Cabecalho titulo="Aprender Next na prática"/>
+           
         </div>
     )
 }
